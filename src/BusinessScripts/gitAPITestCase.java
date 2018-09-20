62c3dd1e6c995ef93be3aa4c79dec06ba9cfe16b
nrramisetty <43371077+nrramisetty@users.noreply.github.com>
2018-09-20 12:56:42 +0530

    added TestNG annotations

--- src/BusinessScripts/gitAPITestCase.java
+++ src/BusinessScripts/gitAPITestCase.java
@@ -1,23 +1,41 @@
 package BusinessScripts;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import org.testng.annotations.Test;
 
 import com.jayway.restassured.response.Response;
 
 import APITests.RestTest;
 
 public class gitAPITestCase {
-	
-	public static void main(String[] args) throws IOException 
-	{
+		
+		/*
+		 * Validate response with AccessToken
+		 */
+		@Test
+		public void ValidateResponsewithAccessToken() throws IOException{
+			
 		RestTest api=new RestTest();
 		Response responce=api.restGet("?access_token=204f0fe28d92a84de888f78de779ba19c10c8564");
-		System.out.println(responce.getStatusCode());
-		System.out.println(responce.asString());
+		System.out.println("Status code is : "+responce.getStatusCode());
+		System.out.println(responce.body().prettyPrint());
 		
+		}
+		
+		/*
+		 * Validate Open Issues 
+		 */
+		
+		@Test
+		public void ValidateOpenIssues() throws IOException{
+			
+		RestTest api=new RestTest();
 		Response responce1=api.restGet("repos/SeleniumHQ/selenium/issues?state=open");
-		System.out.println(responce1.getStatusCode());
-		System.out.println(responce1.asString());
+		System.out.println("Status code is : "+responce1.getStatusCode());
+		System.out.println(responce1.body().prettyPrint());
+		}
 	}
 
-}
+
