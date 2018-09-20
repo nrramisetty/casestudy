5d85206b62a09327221bcef6d7ad5e8c3bdf7cfc
nrramisetty <43371077+nrramisetty@users.noreply.github.com>
2018-09-20 12:26:10 +0530

    deleted comment line

--- src/BusinessScripts/gitAPITestCase.java
+++ src/BusinessScripts/gitAPITestCase.java
@@ -11,7 +11,6 @@ public class gitAPITestCase {
 	public static void main(String[] args) throws IOException 
 	{
 		RestTest api=new RestTest();
-		//api.restPost("Login","/src/test/java/TestData/TestData.json");
 		Response responce=api.restGet("?access_token=204f0fe28d92a84de888f78de779ba19c10c8564");
 		System.out.println(responce.getStatusCode());
 		System.out.println(responce.asString());
